8bc5bee34c5cc3f76d31fe574d510f60fc35cffb
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2022-03-19 17:55:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import sqlite3 as sql
 
 #DEbe poderse registrr un usuario con su nombre y buscalo en el selectbox para poder guardar
 #sus efluentes y ultimos labroatorios
@@ -10,13 +9,14 @@ Registro=st.text_input("Nombre")
 names=nombres.append(Registro)
 
 @st.cache(allow_output_mutation=True)
+def get_data():
+    return []
 
-user_id = st.text_input("Name")
-
+user_id = st.text_input("User ID")
+foo = st.slider("foo", 0, 100)
+bar = st.slider("bar", 0, 100)
 
 if st.button("Add row"):
-    nombres.append({"UserID": user_id, "foo": foo, "bar": bar})
+    get_data().append({"UserID": user_id, "foo": foo, "bar": bar})
 
-st.write(pd.DataFrame(nombres))
-        
-ID=st.selectbox('Busca tu nombre',nombres)
+st.write(pd.DataFrame(get_data()))
